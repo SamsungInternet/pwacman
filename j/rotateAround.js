392ba13f5056e80e1af9ba8fea2809a223e83d14
Diego <diekus@gmail.com>
2017-01-26 18:25:56 +0000

    rollback

--- j/rotateAround.js
+++ j/rotateAround.js
@@ -31,11 +31,11 @@ AFRAME.registerComponent('rotate-around', {
                 break;
             case 'u':
                 if(py <= 3)
-                    py += data.speed*2;
+                    py += 3;
                 break;
             case 'd':
                 if(py >= -3)
-                    py -= data.speed*2;
+                    py -= 3;
                 break;
         }
         var px = data.distance*Math.cos(data.currentAngle%360);
